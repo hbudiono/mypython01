10d0f832947ce4ba6be426f6aa3febb62464400d
Harijoko <harijoko@jsprima.com>
2020-04-18 20:11:40 +0700

    save it

--- turtle01.py
+++ turtle01.py
@@ -1,8 +1,20 @@
 import turtle
-
-turtle.color("blue")
-turtle.stamp
-turtle.fd(50)
-turtle.done
+import random
 
 
+mypet = turtle.Turtle()
+
+mypet.pencolor("yellow")
+mypet.shape('turtle')
+mypet.shapesize(3,3,3)
+mypet.speed(5)
+
+while True :
+  i =  random.randint(1,50)
+  y =  random.randint(1,45)
+  #mypet.penup()
+  mypet.forward(i)
+  mypet.right(y)
+  
+
+turtle.done()
