9ba2b12daa98e77a10cf947000af1bcbd3a07985
Harijoko <harijoko@jsprima.com>
2020-04-21 17:18:33 +0700

    Ganti materia yang baru

---
+++ bab01.py
@@ -0,0 +1,24 @@
+#Comments
+#Comments in Python
+
+
+#Variable with numbers
+a = 10
+b = 20
+
+#Variable with string
+c = "Ini adalah sebuah string"
+d = 'Ini juga sebuah string'
+
+#Variable with lists
+e = [1,2,3,4]
+f = ["A","B,'C"]
+
+#Variable with Tuples
+g = [[1,2],[4,5]]
+h = [["Test","Satu"],["Test Lagi","Dua"]]
+
+print(h)
+# a is a variable to store values
+#Values can be Numbers, Strings or Lists
+
