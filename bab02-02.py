6746db0f39a0d62a43b64f8ee5fc4265456f6216
Harijoko <harijoko@jsprima.com>
2020-04-22 18:08:51 +0700

    rubah comments

--- bab02-02.py
+++ bab02-02.py
@@ -14,13 +14,13 @@ mystring = "Covid-19"
 
 print('====== contoh pass =========')
 for i in range(0,len(mystring)) :
-    if i==3:  #stopper
+    if i==3:  
       pass
     print(i,mystring[i])
 
 print('====== contoh continue =========')
 for i in range(0,len(mystring)) :
-    if i==3:  #stopper
+    if i==3:  
       continue
     print(i,mystring[i])
    
