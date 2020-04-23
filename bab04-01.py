9ad0dd270182e5b995ff7de11f39d8674a06b9b2
Harijoko <harijoko@jsprima.com>
2020-04-23 11:43:38 +0700

    save it

--- bab04-01.py
+++ bab04-01.py
@@ -2,10 +2,26 @@
 
 #Fibonacci series
 
+print("===== Fibonnaci as a procedure ========")
+
 def fib(n):
     a,b=0,1
     while a<n :
         print(a,end=' ')
-        a,b = b,a+b
+        a,b=b,a+b
+    print()
 
 fib(1000)
+
+print("===== Fibonnaci as a function ========")
+
+def ffib(n):
+    result = []
+    a,b=0,1
+    while a<n:
+        result.append(a)
+        a,b=b,a+b
+    return result
+
+print(ffib(1000))
+
