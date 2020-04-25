6428f8cdba54376994b6f4385fa56d3e3b39d871
Harijoko <harijoko@jsprima.com>
2020-04-25 17:23:44 +0700

    adding bab04

--- bab04-01.py
+++ bab04-01.py
@@ -1,3 +1,29 @@
 #more about data structure
+#list functions
+
+
+a=[]
+
+a.append("Apple")
+a.append("Orange")
+a.append("Banana")
+a.append("Pineapple")
+a.append("Apple")
+
+#print before sorting
+print(a)
+a.sort()
+#print after sorting
+print(a)
+
+print(a.count("Apple"))
+a.pop()
+print(a)
+
+
+
+
+
+
 
 
---
+++ bab04-02.py
@@ -0,0 +1,12 @@
+#more about data structure
+#sets
+
+a = {"Test","One","Two"}
+a.add("Three")
+print(a)
+
+b=set("alba gedabra")
+print(b)
+
+c = 'a' in b
+print(c)
