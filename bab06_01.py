869a2d4b28e176fb7acd19854587dc3af38a964b
Harijoko <harijoko@jsprima.com>
2020-04-26 22:00:11 +0700

    save file operation

--- bab06_01.py
+++ bab06_01.py
@@ -1,2 +1,13 @@
 #file operation
 
+f=open('my file.txt','a')
+f.write('This is a test to write to this file\n')
+
+f.close()
+
+f=open('my file.txt','r')
+
+for line in f:
+    print(line,end='')
+
+f.close()
